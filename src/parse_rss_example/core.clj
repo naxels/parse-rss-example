d2aeb7fd8d591082dc3d46e9788012af03c623af
Patrick van de Glind <patrick_de_glind@hotmail.com>
2024-02-25 19:59:13 +0100

    core: cleanup & simplify like core-bb

--- src/parse_rss_example/core.clj
+++ src/parse_rss_example/core.clj
@@ -3,44 +3,25 @@
   (:require [clojure.data.xml :as xml]
             [clojure.java.io :as io]))
 
-;; helper functions
-(defn keyword-in-tag?
-  [keyword entry]
-  (= keyword (:tag entry)))
-
-(def title-tag? (partial keyword-in-tag? :title))
-
-(def item-tag? (partial keyword-in-tag? :item))
-
-(defn get-value
-  [node]
-  (first (:content node)))
-
-(defn rss-title
-  [entry]
-  (get-value (some #(when (title-tag? %) %) (:content entry))))
-
 (defn to-xml
   [uri]
   (-> uri
-      (io/input-stream)
       (xml/parse)
       (xml-seq)))
 
-(defn get-items
-  [xml-seqed]
-  (filter item-tag? xml-seqed))
-
-(defn get-titles
-  [xml-items]
-  (map #(rss-title %) xml-items))
+(defn get-item-titles
+  [seqd-xml]
+  (for [x seqd-xml
+        :let [xc (:content x)
+              content (comp first :content first)] ; fn
+        :when (= :item (:tag x))]
+    (content (filter (comp #{:title} :tag) xc))))
 
 (defn parse-uri-and-grab-titles
   [uri]
-  (->> uri
+  (->> (io/reader uri)
        (to-xml)
-       (get-items)
-       (get-titles)))
+       (get-item-titles)))
 
 ;; main function
 (defn -main
