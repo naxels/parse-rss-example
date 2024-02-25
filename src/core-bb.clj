5ef887576eef74350a745ef8103d5b4c403c1504
Patrick van de Glind <patrick_de_glind@hotmail.com>
2024-02-25 19:54:17 +0100

    core-bb: comment cleanup

--- src/core-bb.clj
+++ src/core-bb.clj
@@ -3,45 +3,11 @@
 (require '[clojure.java.io :as io]
          '[clojure.data.xml :as xml])
 
-;; ;; helper functions
-;; (defn keyword-in-tag?
-;;   [keyword entry]
-;;   (= keyword (:tag entry)))
-
-;; (def title-tag? (partial keyword-in-tag? :title))
-
-;; (def item-tag? (partial keyword-in-tag? :item))
-
-;; (defn get-value
-;;   [node]
-;;   (first (:content node)))
-
-;; (defn rss-title
-;;   [entry]
-;;   ; (get-value (first (filter #(= :title (:tag %)) (:content entry)))))
-;;   ;; (get-value (first (filter title-tag? (:content entry)))))
-;;   (get-value (some #(when (title-tag? %) %) (:content entry))))
-
 (defn to-xml
   [uri]
   (-> uri
       (xml/parse)
       (xml-seq)))
-
-;; (defn get-items
-;;   [xml-seqed]
-;;   (filter item-tag? xml-seqed))
-
-;; (defn get-titles
-;;   [xml-items]
-;;   (map #(rss-title %) xml-items))
-
-;; (defn parse-uri-and-grab-titles
-;;   [uri]
-;;   (->> (io/reader uri)
-;;        (to-xml)
-;;        (get-items)
-;;        (get-titles)))
 
 (defn get-item-titles
   [seqd-xml]
