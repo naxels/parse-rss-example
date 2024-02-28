1ef442ff1f2a2d4f67677db0b149049957cb6dfb
Patrick van de Glind <patrick_de_glind@hotmail.com>
2024-02-28 12:00:02 +0100

    Native image upgrade: make it work with GraalVM 21.0.2

--- build-native.sh
+++ build-native.sh
@@ -1,1 +1,2 @@
-clojure -M:native-image
+# GraalVM 21.0.2
+$GRAALVM_HOME/bin/native-image -jar target/parse-rss-example-*.jar --no-fallback --enable-http --enable-https --features=clj_easy.graal_build_time.InitClojureClasses
