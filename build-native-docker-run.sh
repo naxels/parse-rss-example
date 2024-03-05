51ab79cc7908b7e7d0303ec5bd49bd85e7de5967
Patrick van de Glind <patrick_de_glind@hotmail.com>
2024-03-05 11:39:11 +0100

    build-native-docker-run: fix target

--- build-native-docker-run.sh
+++ build-native-docker-run.sh
@@ -1,4 +1,4 @@
 docker run -it --rm --name graalvm-inst1 \
            -v "$PWD":/target -w /target \
            findepi/graalvm:java21-native \
-           native-image -jar target/test_json-*.jar --no-fallback --enable-https --features=clj_easy.graal_build_time.InitClojureClasses
+           native-image -jar target/parse-rss-example-*.jar --no-fallback --enable-https --features=clj_easy.graal_build_time.InitClojureClasses
